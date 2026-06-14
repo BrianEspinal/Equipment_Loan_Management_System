4e1f85f66dfd08092a245a6703fa6acdd31b573e
BrianEspinal <20251873@itla.edu.do>
2026-06-13 22:49:12 -0400

    CRUD DONE

---
+++ src/Equipment Loan Management System (ELMS)/Entidades/Categoria.cs
@@ -0,0 +1,15 @@
+﻿
+namespace Equipment_Loan_Management_System__ELMS_
+{
+    public class Categoria
+    {
+        public int Id { get; set; }
+
+        // Evitamos el CS8618 inicializando con string.Empty
+        public string Nombre { get; set; } = string.Empty;
+        public string Descripcion { get; set; } = string.Empty;
+
+        // Propiedad que aparece en tu captura de Swagger
+        public bool IsActive { get; set; } = true;
+    }
+}
