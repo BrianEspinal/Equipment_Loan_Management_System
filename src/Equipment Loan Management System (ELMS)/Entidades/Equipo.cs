87c9db1a7a053125d3dc02beb8edc7c5811e6567
Brian Espinal P. <20251873@itla.edu.do>
2026-06-24 00:03:37 -0400

    Refactor Equipo class properties and comments

--- src/Equipment Loan Management System (ELMS)/Entidades/Equipo.cs
+++ src/Equipment Loan Management System (ELMS)/Entidades/Equipo.cs
@@ -8,17 +8,17 @@ namespace Equipment_Loan_Management_System__ELMS_
         public string Nombre { get; set; } = string.Empty;
         public string Marca { get; set; } = string.Empty;
 
-        // En tu Swagger dice "nullable: true", así que usamos el signo '?'
+       
         public string? NumeroSerie { get; set; }
         public string? Estado { get; set; } = "Disponible";
 
         public DateTime FechaRegistro { get; set; } = DateTime.Now;
         public int CategoriaId { get; set; }
 
-        // Propiedad de navegación mapeada en tu Swagger
+        // Propiedad de navegación mapeada en Swagger
         public Categoria? Categoria { get; set; }
 
-        // Propiedad activa mapeada en tu Swagger
+       
         public bool IsActive { get; set; } = true;
     }
 }
