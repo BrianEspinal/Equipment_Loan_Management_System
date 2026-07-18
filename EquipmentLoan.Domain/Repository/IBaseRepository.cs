5a18238fb74862667cc494d1ac98fb17b9d2794d
BrianEspinal <20251873@itla.edu.do>
2026-07-18 19:59:06 -0400

    Creando las interfaces para los CRUD principales
    herencia de interfaces

--- EquipmentLoan.Domain/Repository/IBaseRepository.cs
+++ EquipmentLoan.Domain/Repository/IBaseRepository.cs
@@ -1,10 +1,22 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Text;
+﻿using EquipmentLoan.Domain.Core;
 
-namespace EquipmentLoan.Domain.Repository
+namespace EquipmentLoan.Domain.Repository;  
+
+public interface IBaseRepository<TEntity>
+    where TEntity : BaseEntity
 {
-    internal class IBaseRepository
-    {
-    }
+    Task<List<TEntity>> GetAllAsync();
+
+    Task<TEntity?> GetByIdAsync(int id);
+
+    Task AddAsync(TEntity entity);
+
+    void Update(TEntity entity);
+
+    void Delete(TEntity entity);
+
+    Task<int> SaveChangesAsync();
 }
+/// Define las operaciones CRUD comunes que deben implementar
+/// los repositorios de las entidades que heredan de BaseEntity y 
+/// las guarda asi no necesito un UnitofWork
--- EquipmentLoan.Domain/Repository/IEquipmentRepository.cs
+++ EquipmentLoan.Domain/Repository/IEquipmentRepository.cs
@@ -1,10 +1,17 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Text;
+﻿using EquipmentLoan.Domain.Entities;
 
-namespace EquipmentLoan.Domain.Repository
+namespace EquipmentLoan.Domain.Repository;
+
+
+public interface IEquipmentRepository : IBaseRepository<Equipment>
 {
-    internal class IEquipmentRepository
-    {
-    }
+    Task <Equipment?> GetByInventoryCodeAsync  //busca un equipo por su código de inventario, devuelve null si !=.
+        (string inventoryCode);
+
+    Task<List  <Equipment>>   GetAvailableAsync();  //busca los equipos 'available' para préstamo
+
+    Task<bool> InventoryCodeExistsAsync
+        (string inventoryCode);
+
+   ///seRepository en Infrastructure contiene cómo funciona realmente mediante Entity Framework.en ELoan.Infrastructure/core/BaseRepository.cs
 }
