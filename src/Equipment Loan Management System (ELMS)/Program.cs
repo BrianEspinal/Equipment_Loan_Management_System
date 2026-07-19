cdef6972be066c9e750e406bb163d9e67cc9aa01
BrianEspinal <20251873@itla.edu.do>
2026-07-19 01:23:22 -0400

    Update Program.cs

--- src/Equipment Loan Management System (ELMS)/Program.cs
+++ src/Equipment Loan Management System (ELMS)/Program.cs
@@ -2,13 +2,21 @@ using EquipmentLoan.Domain.Repository;
 using EquipmentLoan.Infrastructure.Repositories;
 using EquipmentLoan.Infrastructure.Context;
 using Microsoft.EntityFrameworkCore;
+using EquipmentLoan.Application.Contract;
+using EquipmentLoan.Application.Service;
+
 /// ya para este punto miAPI conoce a infraestructura y el contexto de base de datos.
 
 var builder = WebApplication.CreateBuilder(args);   
 builder.Services.AddDbContext<EquipmentLoanContext>(options =>  //172 Registramos context en el sistema de inyección de dependencias de ASP.NET.
     options.UseSqlServer(   // 111 Ussamos SQL Server como proveedor de base de datos
         builder.Configuration.GetConnectionString(
-            "EquipmentLoanConnection")));  
+            "EquipmentLoanConnection")));
+
+
+builder.Services.AddScoped<IEquipmentRepository, EquipmentRepository>();
+builder.Services.AddScoped<IEquipmentService, EquipmentService>();
+
 
 builder.Services.AddScoped<IEquipmentRepository, EquipmentRepository>();  // 172 inyección de dependencias en EquipmentRepository desde IEquipmentRepository.
 
