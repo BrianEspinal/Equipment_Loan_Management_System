d19ddde5f29ab0bf57f32d58fb49b2a45c38d1c1
Brian Espinal P. <20251873@itla.edu.do>
2026-08-20 15:37:05 -0400

    Delete Categoria class from ELMS
    Remove Categoria class and its properties.

--- src/Equipment Loan Management System (ELMS)/Entidades/Categoria.cs
+++ src/Equipment Loan Management System (ELMS)/Entidades/Categoria.cs
@@ -1,15 +1,1 @@
-﻿
-namespace Equipment_Loan_Management_System__ELMS_
-{
-    public class Categoria
-    {
-        public int Id { get; set; }
 
-        // Evitamos el error CS8618 definidendo el tipo de dato de nombre y string  al inicializar con string.Empty
-        public string Nombre { get; set; } = string.Empty;
-        public string Descripcion { get; set; } = string.Empty;
-
-      
-        public bool IsActive { get; set; } = true;
-    }
-}
